2dbed5a5dcadce89287e35644533bbbe06436405
Uriel Shapiro <uriel1999@gmail.com>
2024-05-23 16:53:25 +0300

    Added demo from previous execersice

--- Demo.cpp
+++ Demo.cpp
@@ -58,4 +58,68 @@ int main()
     {
         cout << e.what() << endl; // Should print "The number of columns in the first matrix must be equal to the number of rows in the second matrix."
     }
+
+    ///////////////////////////// Demo from previous exercise /////////////////////////////
+
+    ariel::Graph g;
+    // 3x3 matrix that represents a connected graph.
+    vector<vector<int>> graph3 = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 1, 0}};
+    g.loadGraph(graph3); // Load the graph to the object.
+
+    cout << Algorithms::isConnected(g) << endl;        // Should print: "1" (true).
+    cout << Algorithms::shortestPath(g, 0, 2) << endl; // Should print: 0->1->2.
+    cout << Algorithms::isContainsCycle(g) << endl;    // Should print: "0" (false).
+    cout << Algorithms::isBipartite(g) << endl;        // Should print: "The graph is bipartite: A={0, 2}, B={1}."
+    cout << endl;
+
+    // 5x5 matrix that represents a non-connected graph with a cycle.
+    vector<vector<int>> graph4 = {
+        {0, 1, 1, 0, 0},
+        {1, 0, 1, 0, 0},
+        {1, 1, 0, 1, 0},
+        {0, 0, 1, 0, 0},
+        {0, 0, 0, 0, 0}};
+
+    g.loadGraph(graph4); // Load the graph to the object.
+
+    cout << Algorithms::isConnected(g) << endl;        // Should print: "0" (false).
+    cout << Algorithms::shortestPath(g, 0, 4) << endl; // Should print: "-1" (there is no path between 0 and 4).
+    cout << Algorithms::isContainsCycle(g) << endl;    // Should print: "The cycle is: 0->1->2->0".
+    cout << Algorithms::isBipartite(g) << endl;        // Should print: "0" (false).
+    cout << endl;
+
+    // 5x5 matrix that reprsents a connected weighted graph.
+    vector<vector<int>> graph5 = {
+        {0, 1, 2, 0, 0},
+        {1, 0, 3, 0, 0},
+        {2, 3, 0, 4, 0},
+        {0, 0, 4, 0, 5},
+        {0, 0, 0, 5, 0}};
+    g.loadGraph(graph5); // Load the graph to the object.
+
+    cout << Algorithms::isConnected(g) << endl;        // Should print: "1" (true).
+    cout << Algorithms::shortestPath(g, 0, 4) << endl; // Should print: 0->2->3->4.
+    cout << Algorithms::isContainsCycle(g) << endl;    // Should print: "0->1->2->0" (true).
+    cout << Algorithms::isBipartite(g) << endl;        // Should print: "0"
+    cout << endl;
+
+    // 5x4 matrix that reprsents invalid graph.
+    vector<vector<int>> graph6 = {
+        {0, 1, 2, 0},
+        {1, 0, 3, 0},
+        {2, 3, 0, 4},
+        {0, 0, 4, 0},
+        {0, 0, 0, 5}};
+    try
+    {
+        g.loadGraph(graph6); // Load the graph to the object.
+    }
+    catch (const std::invalid_argument &e)
+    {
+        cout << e.what() << endl; // Should print: "Invalid graph: The graph is not a square matrix."
+    }
+    return 0;
 }
